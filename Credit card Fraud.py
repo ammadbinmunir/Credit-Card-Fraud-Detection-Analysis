6bf79b4de546f5c548a0305fb62358465501d95d
Ammad Bin Munir <125456032+ammadbinmunir@users.noreply.github.com>
2023-11-15 13:08:28 +0000

    Add files via upload
    under analysis yet

---
+++ Credit card Fraud.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[50]:
+
+
+#importing pandas and numpy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import precision_score, recall_score, f1_score
+from scipy.stats import ks_2samp
+
+
+# In[51]:
+
+
+#importing credit fraud CSV file and recalling it
+df= pd.read_csv("C:\\Users\\Ammad\OneDrive\\Desktop\\BPP University\\other courses\\Machine Learning 1\\creditcard.csv")
+df
+
+
+# In[52]:
+
+
+
+#class colums the status of the transaction weather it was a fraud or it was a real transaction.
+#we will target the class
+target= "Class"
+df[target].value_counts()
+
+
+# In[53]:
+
+
+# Set the aesthetic style of the plots
+sns.set_style("darkgrid")
+#plotting graph
+f,(axis1,axis2)=plt.subplots(2,1, sharex= True, figsize=(11,5))
+bins = 70
+
+# Plotting for Fraud transactions
+axis1.hist(df.Time[df.Class == 1], bins=bins, color='red', alpha=0.7)
+axis1.set_title('Fraud', fontsize=14)
+axis1.set_ylabel('Transaction Count', fontsize=12)
+axis1.grid(True, linestyle='-', alpha=0.5)
+
+# Plotting for Normal transactions
+axis2.hist(df.Time[df.Class == 0], bins=bins, color='green', alpha=0.7)
+axis2.set_title('Normal', fontsize=14)
+axis2.set_xlabel('Time (in Seconds)', fontsize=12)
+axis2.set_ylabel('Transaction Count', fontsize=12)
+axis2.grid(True, linestyle='-', alpha=0.5)
+
+# Remove top and right spines
+for axis in [axis1, axis2]:
+    axis.spines['top'].set_visible(False)
+    axis.spines['right'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
+
+
+# In[67]:
+
+
+#installing snap lib for data cleaning 
+get_ipython().system('pip -q install snaplib')
+get_ipython().system('pip install termcolor')
+get_ipython().system('pip install lightgbm')
+
+
+# In[68]:
+
+
+#loading snaplib libraray
+from snaplib.snaplib import Snaplib
+
+
+# In[73]:
+
+
+#data cleaning to eliminate irrelevant values
+#sl needs to be insatlled to clean data 
+df=Snaplib.cleane(df,target=target,verbose= True)
+df[target].value_counts
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
